a470e5ddff58b644ad32e41258fcf93a73041c45
Yves LMDG <yves.lmdg@sfr.fr>
2022-01-24 11:21:00 +0100

    Exo 2

---
+++ src/main/java/ex2/CompteCourant.java
@@ -0,0 +1,45 @@
+package ex2;
+
+/**
+ * Classe représantant un compte bancaire ayant un découvert autorisé
+ * 
+ * @author YvesLMDG
+ *
+ */
+public class CompteCourant extends CompteBancaire{
+
+	/**
+	 * Découvert authorisé
+	 */
+	private double decouvert;
+
+	/**
+	 * Créé un compte bancaire avec un solde initial et son découvert autorisé
+	 * 
+	 * @param solde
+	 * @param decouvert
+	 */
+	public CompteCourant(double solde, double decouvert) {
+		super(solde);
+	}
+	
+	/**
+	 * Débite @param montant au solde si le solde reste supérieur au découvert authorisé
+	 */
+	public void debiterMontant(double montant){
+		double soldeTemp = getSolde() - montant;
+		if (soldeTemp < getDecouvert()) {
+			setSolde(soldeTemp);
+		}
+	}
+	
+	public double getDecouvert() {
+		return decouvert;
+	}
+	
+	public void setDecouvert(double decouvert) {
+		this.decouvert = decouvert;
+	}
+
+	
+}
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,56 @@
 package ex2;
 
+
+/**
+ * Classe Représantant un compte bancaire avec un taux de rémunération
+ *  annuel sans découvert aurtorisé
+ *   
+ * @author YvesLMDG
+ *
+ */
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/**
+	 * Taux de rémunération annuel
+	 */
+	private double tauxRemuneration;
+	
+	/**
+	 * Créé un compte bancaire avec un solde initial et son taux de rémunération annuel
+	 * 
+	 * @param solde
+	 * @param tauxRemuneration
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde);
+		this.tauxRemuneration=tauxRemuneration;
 	}
 
+	/**
+	 * Débite @param montant du solde si le solde reste positif
+	 */
+	public void debiterMontant(double montant){
+		double soldeTemp = calculDebit(montant);;
+		if (soldeTemp > 0){
+			setSolde(soldeTemp);
+
+		}
+	}
+	
+	/**
+	 * Calcul et applique au solde du compte le taux de rémunération annuel
+	 */
+	public void appliquerRemuAnnuelle(){
+		setSolde(getSolde()*getTauxRemuneration()/100);
+
+	}
+
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
+	
 }
